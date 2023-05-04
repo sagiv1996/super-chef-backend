8895d6372740c65ff0e13ffb9959e2642f0f8d2b
sagiv bonali <sagivb13@gmail.com>
2023-05-04 12:30:21 +0300

    Update shopping-list schema

--- schemas/shopping-list.schema.ts
+++ schemas/shopping-list.schema.ts
@@ -1,18 +1,36 @@
 import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 
 export type ShoppingListDocument = HydratedDocument<ShoppingList>;
 
 @Schema()
 export class ShoppingList {
-  @Prop({ required: true })
+  @Prop({ required: true, type: String })
   ownerId: string;
 
   @Prop({ required: true })
   name: string;
 
-  @Prop({ required: true, default: false })
-  isCompleted: boolean;
+  @Prop({ type: String })
+  sharedWith: string[];
+
+  @Prop({
+    type: [
+      {
+        id: { type: Types.ObjectId, ref: 'Ingredient', required: true },
+        amount: { type: Number, min: 1, required: true },
+        isBought: { type: Boolean, default: false, required: true },
+      },
+    ],
+    _id: false,
+  })
+  ingredients: [
+    {
+      id: { type: Types.ObjectId; ref: 'Ingredient' };
+      amount: number;
+      isBought: boolean;
+    },
+  ];
 }
 
 export const ShoppingListSchema = SchemaFactory.createForClass(ShoppingList);
